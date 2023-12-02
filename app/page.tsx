763ee8126c139b4ad7f43e940e27791560b11bb1
S. Sifantus <hi@ssifantus.com>
2023-12-01 16:40:13 -0800

    homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Column from "components/Column";
 
 
 export default function Home() {
   return (
-    <div className="page">
-      Home
+    <div className="selector">
+      <Column type={"characters"}/>
+      <Column type={"words"}/>
+      <Column type={"sentences"}/>
+      <Column type={"paragraphs"}/>
     </div>
   );
 }
