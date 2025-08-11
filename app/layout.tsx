7ce2fe5bfdf7becf0555db6903c45e4a0b1abfec
S. Sifantus <hi@ssifantus.com>
2025-08-11 16:18:58 -0700

    dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,14 +21,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-blue-900">
-                <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                        <Header />
+            <body className="antialiased">
                         <main className="grow">{children}</main>
-                        <Footer />
-                    </div>
-                </div>
+
             </body>
         </html>
     );
