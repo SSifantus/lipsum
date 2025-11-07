1effbf7999a663d2f308c10b9706bffbedb64491
S. Sifantus <hi@ssifantus.com>
2025-11-07 09:14:07 -0800

    add min max

--- components/selection-surface.tsx
+++ components/selection-surface.tsx
@@ -1,4 +1,3 @@
-
 import { SelectionPane } from "./selection-pane";
 
 
@@ -6,27 +5,40 @@ const panes = [
   {
     title: "Characters",
     value: "characters",
+    step: 1,
+    min: 0,
+    max: 5000,
   },
   {
     title: "Words",
     value: "words",
+    step: 1,
+    min: 0,
+    max: 2000,
   },
   {
     title: "Sentences",
     value: "sentences",
+    step: 1,
+    min: 0,
+    max: 100,
   },
   {
     title: "Paragraphs",
     value: "paragraphs",
+    step: 1,
+    min: 0,
+    max: 100,
   },
 ];
 
 export function SelectionSurface() {
   return (
     <div className="w-full h-full grid grid-cols-4 grid-rows-1 pb-20">
-      {panes.map( ( pane ) => (
-        <SelectionPane key={pane.title} title={pane.title} value={pane.value} onChange={() => { }} />
-      ) )}
+      {panes.map((pane) => (
+        <SelectionPane key={pane.title} title={pane.title} value={pane.value} onChange={() => {
+        }}/>
+      ))}
     </div>
   );
 }
