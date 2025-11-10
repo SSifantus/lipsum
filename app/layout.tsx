548c0f05f3307164467f2060449f6c76a6c65900
S. Sifantus <hi@ssifantus.com>
2025-11-10 13:58:21 -0800

    remove custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,32 @@
 import { Footer, Header, ThemeProvider, Toaster } from "@/components";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { ReactNode } from "react";
 import "@/styles/globals.css";
-
-const geistSans = Geist( {
-  variable: "--font-geist-sans",
-  subsets: [ "latin" ],
-} );
-
-const geistMono = Geist_Mono( {
-  variable: "--font-geist-mono",
-  subsets: [ "latin" ],
-} );
 
 export const metadata: Metadata = {
   title: "Lipsum Pro",
   description: "Lorem Ipsum generator",
 };
 
-export default function RootLayout( {
-  children,
-}: Readonly<{
+export default function RootLayout({
+                                     children,
+                                   }: Readonly<{
   children: ReactNode;
-}> ) {
+}>){
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`min-h-screen bg-background font-sans antialiased ${geistSans.variable} ${geistMono.variable}`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Toaster />
-          <Header />
-          {children}
-          <Footer />
-        </ThemeProvider>
-      </body>
-      <GoogleAnalytics gaId="G-" />
+    <body
+      className="min-h-screen bg-background font-sans antialiased"
+    >
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <Toaster/>
+      <Header/>
+      {children}
+      <Footer/>
+    </ThemeProvider>
+    </body>
+    <GoogleAnalytics gaId="G-"/>
     </html>
   );
 }
