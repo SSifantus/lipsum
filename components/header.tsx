fcc70fd4406735948b65b39d4949470ebc694ab1
S. Sifantus <hi@ssifantus.com>
2025-12-19 17:38:44 -0800

    styling

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,16 @@
-import { Logo } from "@/components/logo";
-import { SourceSelector } from "@/components/source-selector";
-import { ThemeToggler } from "@/components/theme-toggler";
+import {Logo} from "@/components/logo";
+import {SourceSelector} from "@/components/source-selector";
+import {ThemeToggler} from "@/components/theme-toggler";
 
-export function Header(){
+export function Header() {
   return (
     <header
-      className="fixed left-0 top-0 z-50 w-full px-4 animate-fade-in border-b border-border backdrop-blur-[12px] [--animation-delay:600ms]">
+      className="fixed left-0 top-0 z-50 w-full px-4 pr-2.5 animate-fade-in border-b border-border backdrop-blur-[12px] [--animation-delay:600ms]">
       <div className="flex h-[var(--navigation-height)] w-full items-center justify-between">
-        <Logo/>
+        <Logo />
         <div className="flex gap-3.5">
-          <SourceSelector/>
-          <ThemeToggler/>
+          <SourceSelector />
+          <ThemeToggler />
         </div>
       </div>
     </header>
